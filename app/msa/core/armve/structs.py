ca1ad48c82dd31a41673f6d4441f55a6fbf58694
johntitorpy <133544298+johntitorpy@users.noreply.github.com>
2023-05-14 03:43:51 +0000

    s

---
+++ app/msa/core/armve/structs.py
@@ -0,0 +1,8 @@
+struct_tag_header = Struct("tag_header",
+                           UBInt8("token"),
+                           UBInt8("tipo_tag"),
+                           UBInt16("size"))
+struct_tag = Struct("tag",
+                    Embed(struct_tag_header),
+                    Bytes("crc32", 4),
+                    Bytes("user_data", lambda ctx: ctx.size))
